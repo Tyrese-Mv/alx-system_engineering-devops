fff7debe221b6cc8ccd2c4d09a517b37fa909f46
Tyrese-Mv <musawenkosi106@gmail.com>
2024-01-22 22:58:43 +0200

    added task 2 fix 1

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #  manifest that kills a process named killmenow
 
-exec { 'kill_process_killmenow':
-  command     => 'pkill -f killmenow',
-  refreshonly => true,
+exec { 'killmenow':
+  command => 'pkill -f killmenow',
+  path    =>  '/bin:usr/bin',
 }
